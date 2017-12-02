e8d68487cbefed94c7276b54706b9b26d5704fe8
salomon <xploralog@hotmail.com>
2017-12-02 18:03:30 -0500

    Mi primer menu

---
+++ app/src/main/java/com/salomon/ejemplomenus/MainActivity.java
@@ -0,0 +1,61 @@
+package com.salomon.ejemplomenus;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu m)
+    {
+        getMenuInflater().inflate(R.menu.menu1,m);
+        return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+
+    {
+
+        switch (item.getItemId())
+        {
+            case R.id.opcion1:
+                Intent intento1 = new Intent (this,MainActivity.class);
+                startActivity(intento1);
+                return true;
+
+            case R.id.opcion2:
+                Intent intento2 = new Intent (this,Main2Activity.class);
+                startActivity(intento2);
+                return true;
+
+            case R.id.opcion3:
+                Intent intento3 = new Intent (this,Main3Activity.class);
+                startActivity(intento3);
+                return true;
+
+
+
+                default:
+                     return  super.onOptionsItemSelected(item);
+
+
+        }
+
+    }
+
+
+
+
+
+
+}
